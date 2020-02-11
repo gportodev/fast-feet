51ba878400249814b87cc962abad771eaeda75a2
Gamonpo <gamonpo@outlook.com>
2020-02-11 11:23:02 -0300

    Insere confirmPassword em schema

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -46,6 +46,7 @@ class UserController {
       email: Yup.string().email(),
       oldPassword: Yup.string().min(6),
       password: Yup.string().min(6).when('oldPassword', (oldPassword, field) => (oldPassword ? field.required() : field)),
+      confirmPassword: Yup.string().when('password', (password, field) => (password ? field.required().oneOf([Yup.ref('password')]) : field)),
     });
 
     // Valida dados de entrada com base no schema
