3f55a7c859337e6ddfc2d768c5c2da4140118ce0
Gamonpo <gamonpo@outlook.com>
2020-02-12 15:28:01 -0300

    Valida dados de entrada

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,29 @@
+import * as Yup from 'yup'; //* as *var* -> recebe tudo de yup
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    /* Cria schema de validação
+    Qual a forma do objeto?
+    Quais os campos?
+    */
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      complement: Yup.string().required(),
+      state: Yup.string().required(),
+      city: Yup.string().required(),
+      zip_code: Yup.string().required(),
+    });
+
+    // Valida dados de entrada com base no schema
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Dados incorretos!' });
+    }
+
     const recipientExists = await Recipient.findOne({ where: { name: req.body.name } });
 
     if (recipientExists) {
